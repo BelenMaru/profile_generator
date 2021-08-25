229f850d86f81e58dab41d26d28d5010c0751a2a
BelenMaru <bleneee@gmail.com>
2021-08-25 13:23:03 +0000

    add fun proflile generator

---
+++ survey.js
@@ -0,0 +1,30 @@
+// survey app which asks the user a bunch of questions like their favourite music, activity, food, sport, etc. 
+// It will then generate a profile description to use online
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+rl.question(`What's your name?  `, (name) => {
+  rl.question(`What's an activity you like doing?  `, (activity) => {
+    rl.question(`What do you listen to while doing that?  `, (music) => {
+      rl.question(`Which meal is your favourite (eg: dinner, brunch, etc.)  `, (meal) => {
+        rl.question(`What's your favourite thing to eat for that meal?  `, (eat) => {
+          rl.question(`Which sport is your absolute favourite?  `, (sport) => {
+            rl.question(`What is your superpower? In a few words, tell us what you are amazing at!  `, (power) => {
+              console.log(`${name}, loves listening to ${music} while ${activity}, starving ${eat} for ${meal}, want  ${sport} to to stay fit and get ${power}`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
